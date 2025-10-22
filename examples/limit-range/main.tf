404a995c868a3a63cb78788625d81130998ef54d
Divya Kamath <99815425+dimallya@users.noreply.github.com>
2025-10-22 17:02:41 +0530

    Update main.tf

--- examples/limit-range/main.tf
+++ examples/limit-range/main.tf
@@ -70,3 +70,56 @@ resource "kubernetes_deployment" "insecure_app" {
     }
   }
 }
+
+provider "kubernetes" {}
+
+resource "kubernetes_pod" "nginx" {
+  metadata {
+    name = "nginx-pod"
+
+    labels {
+      app = "nginx"
+    }
+  }
+
+  spec {
+    container {
+
+      # Missing or incorrect run_as_non_root setting
+      security_context {
+        run_as_user = 0
+      }
+
+      image = "nginx:1.7.8"
+      name  = "nginx"
+
+      port {
+        container_port = 80
+      }
+    }
+  }
+}
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx"
+  }
+
+  spec {
+    selector {
+      # NGINX pod labels are being referenced
+      app = "${kubernetes_pod.nginx.metadata.0.labels.app}"
+    }
+
+    port {
+      port        = 80
+      target_port = 80
+    }
+
+    type = "${var.service_type}"
+  }
+}
+
+variable "service_type" {
+  default = "LoadBalancer"
+}
